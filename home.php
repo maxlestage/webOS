78c4e40e4585880042146740d31e6530a7d4b3fe
Max Lestage <maxlestage@icloud.com>
2020-08-13 00:48:41 +0200

    first commit

---
+++ home.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+    <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/notepad.css"> 
+    <link rel="stylesheet" href="assets/css/dock.css"> 
+    <link rel="stylesheet" href="assets/css/style.css"> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>WebOS</title>
+    </head>
+    <body>
+        <header>
+        <nav class="navbar navbar-expand-lg navbar-light ">
+  <a class="navbar-brand" href="#"><img src="assets/images/1009px-Apple_logo_black.svg.png" width="30" height="30" alt=""></a>
+  
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+        </header>
+        <div class='wrapper'>
+            <div id='full-image'></div>
+        </div>
+        <div class="notepad">
+                <div class="top"></div>
+                    <div class="paper" contenteditable="true">
+                        Web Os<br>
+                        Alpha 00.1
+                    </div>
+            </div>
+                <div id="dock-container">
+                    <div id="dock"> 
+                    <ul>
+                        <li>
+                            <span>Mail</span>
+                            <a href="https://www.icloud.com/mail/"><img src="assets/images/r$__62.png"/></a>
+                        </li>
+                        <li>
+                            <span>Contact</span>
+                            <a href="https://www.icloud.com/contacts/"><img src="assets/images/r$__64.png"/></a>
+                        </li>
+                        <li>
+                            <span>Photos</span>
+                            <a href="https://www.icloud.com/photos/"><img src="assets/images/r$__68.png"/></a>
+                        </li>
+                        <li>
+                            <span>iCloud Drive</span>
+                            <a href="https://www.icloud.com/iclouddrive/"><img src="assets/images/r$__51.png"/></a>
+                        </li>
+                        <li>
+                            <span>Discord</span>
+                            <a href="https://discord.com/login"><img src="assets/images/discord-logo-logodownload-download-logotipos-1.png"/></a>
+                        </li>
+                        <li>
+                            <span>Flickr</span>
+                            <a href="https://www.flickr.com/photos/maxlstage/albums"><img src="assets/images/flickr_icon-icons.com_69254.png"/></a>
+                        </li>
+                        <li>
+                            <span>Musique</span>
+                            <a href="https://music.apple.com/fr/for-you"><img src="assets/images/langfr-440px-ITunes_logo.svg.png"/></a>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    </body>
+</html>
